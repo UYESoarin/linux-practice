d6617c0f7a3ca62bfba97c35e30f27a0e685b1fe
UYE <uye_soarin@qq.com>
2026-09-18 21:02:14 +0800

    Refactor common.h header file includes
    Updated includes and added comments for clarity.

--- linux-lab/lab1/common.h
+++ linux-lab/lab1/common.h
@@ -3,11 +3,11 @@
 #define COMMON_H
 
 #include <stdio.h>  // standard I/O
-#include <stdlib.h>  // exit
+#include <stdlib.h>  // exit, EXIT_FAILURE
 #include <unistd.h>  // UNIX: fork, execl, sleep, getpid
 #include <signal.h>  // signal, sigaction, sigemptyset, kill
 #include <sys/types.h>  // system types: pid_t
-#include <sys/wait.h>  // wait, waitpit, WIFEXITED, WEXITSTATUS
-#include <errno.h>  // EINTR
+#include <sys/wait.h>  // wait, waitpid, WIFEXITED, WEXITSTATUS, WTREMSIG, WIFSIGNALED
+#include <errno.h>  // erron, EINTR
 
 #endif
